70c9a2f4aa12fde6f21a4e540fd7ad3a0479445e
4hel <4hel@posteo.de>
2016-11-28 19:03:26 +0100

    n! recursion

--- chap4ex2.s
+++ chap4ex2.s
@@ -1,48 +1,33 @@
-	# PURPOSE: compute 2^3 + 5^2
+	# PURPOSE: recursively compute the factorial of the number 4 (4*3*2*1)
 	#
-	# VARIABLES:
-	# 	
 	.code32
 	.section .data	
 	.section .text
 	.globl _start
-	.globl power
 
 _start:
 	
-	pushl $3			# 2. Parameter
-	pushl $2			# 1. Parameter
-	call power
-	addl $8, %esp			# move the stack pointer back
-	pushl %eax			# store retval of 2^3
-	pushl $2			# 2. Parameter
-	pushl $5			# 1. Parameter
-	call power
-	addl $8, %esp			# move the stack pointer back
-	popl %ebx			# get first retval from stack
-	addl %eax, %ebx			# add both powers for return
+	pushl $4			# function parameter
+	call factorial
+	addl $4, %esp			# remove param from stack
+	movl %eax, %ebx			# funcion reval -> prog retval
 exit_prog:
 	movl $1, %eax			# exit system call
 	int $0x80
 	
-	.type power, @function
-power:					# BEGIN FUNCTION
-	pushl %ebx			# 1. save old base pointer
+	.type factorial, @function
+factorial:				# BEGIN FUNCTION
+	pushl %ebp			# 1. save old base pointer
 	movl %esp, %ebp			# 2. make stack pointer the base pointer
-					
-	movl 8(%ebp), %ebx		# move 1. Parameter to %eax
-	movl 12(%ebp), %ecx		# move 2. Parameter to %ebx
-	pushl %ebx			# init local_var1 with base
-power_loop:
-	cmpl $1, %ecx			# while( power != 1)
-	je power_end
-	movl -4(%ebp), %eax		# local_var1 -> %eax
-	imull %ebx, %eax		# base * local_var1 
-	movl %eax, -4(%ebp)		# local_var1 := mul_result
-	decl %ecx			# power--
-	jmp power_loop
-	
-power_end:	
+	movl 8(%ebp), %eax		# move parameter to %eax
+	cmpl $1, %eax			# n == 1 ? -> base case
+	je factorial_end		# then return withour new recursion
+	decl %eax			# n = n -1
+	pushl %eax
+	call factorial
+	movl 8(%ebp), %ebx
+	imul %ebx, %eax
+factorial_end:	
 	movl %ebp, %esp			# restore stack pointer
 	popl %ebp			# restore base pointer
 	ret				# return control	
