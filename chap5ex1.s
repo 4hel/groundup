dad7898ac626ccfbbb177e0e3376a82c93639e86
4hel <4hel@posteo.de>
2016-11-30 20:05:31 +0100

    in out loop

--- chap5ex1.s
+++ chap5ex1.s
@@ -1,48 +1,175 @@
-	# PURPOSE: compute 2^3 + 5^2
+	# PURPOSE: convert input file to output file making all letters uppercase
 	#
-	# VARIABLES:
-	# 	
+	# ./prog README.md out.txt
+	#
 	.code32
-	.section .data	
-	.section .text
-	.globl _start
-	.globl power
 
-_start:
+	#
+	# DATA
+	#
+	.section .data
 	
-	pushl $3			# 2. Parameter
-	pushl $2			# 1. Parameter
-	call power
-	addl $8, %esp			# move the stack pointer back
-	pushl %eax			# store retval of 2^3
-	pushl $2			# 2. Parameter
-	pushl $5			# 1. Parameter
-	call power
-	addl $8, %esp			# move the stack pointer back
-	popl %ebx			# get first retval from stack
-	addl %eax, %ebx			# add both powers for return
+	# system calls
+	.equ SYS_OPEN, 5
+	.equ SYS_WRITE, 4
+	.equ SYS_READ, 3
+	.equ SYS_CLOSE, 6
+	.equ SYS_EXIT, 1
+	.equ LINUX_SYSCALL, 0x80
+
+	# options for open from /usr/include/asm-generic/fcntl.h
+	.equ O_RDONLY, 0
+	.equ O_CREAT_WRONLY_TRUNC, 03101
+
+	# standard file descriptors
+	.equ STDIN, 0
+	.equ STDOUT, 1
+	.equ STDERR, 2
+
+	# misc
+	.equ END_OF_FILE, 0		# retval of read?
+	.equ NUMBER_ARGUMENTS, 2
+
+	#
+	# BUFFER 
+	#
+	.section .bss
+	.equ BUFFER_SIZE, 500
+	.lcomm BUFFER_DATA, BUFFER_SIZE
+	
+	#
+	# PROGRAM
+	#
+	.section .text
+
+	# stack postions
+	.equ ST_SIZE_RESERVE, 8
+	.equ ST_FD_IN, -4
+	.equ ST_FD_OUT, -8
+	.equ ST_ARGC, 0			# number of arguments
+	.equ ST_ARGV_0, 4		# name of program
+	.equ ST_ARGV_1, 8		# input file name
+	.equ ST_ARGV_2, 12		# output file name
+	
+	.globl _start
+_start:
+	movl %esp, %ebp			# save the stack pointer
+	subl $ST_SIZE_RESERVE, %esp
+
+open_fd_in:	
+	### open input file
+	movl $SYS_OPEN, %eax		# open
+	movl ST_ARGV_1(%ebp), %ebx	# file name
+	movl $O_RDONLY, %ecx		# read only
+	movl $0666, %edx		# permission not really needed
+	int $LINUX_SYSCALL
+	#
+	movl %eax, ST_FD_IN(%ebp)	# returned file descriptor saved on stack
+
+	### open output file
+	movl $SYS_OPEN, %eax		# open
+	movl ST_ARGV_2(%ebp), %ebx	# file name
+	movl $O_CREAT_WRONLY_TRUNC, %ecx # write only
+	movl $0666, %edx		# permission set for new file
+	int $LINUX_SYSCALL
+	#
+	movl %eax, ST_FD_OUT(%ebp)	# returned file descriptor saved on stack
+
+read_loop_begin:
+	
+	### read from input file
+	movl $SYS_READ, %eax
+	movl ST_FD_IN(%ebp), %ebx
+	movl $BUFFER_DATA, %ecx
+	movl $BUFFER_SIZE, %edx
+	int $LINUX_SYSCALL
+
+	### check for end of file
+	cmpl $END_OF_FILE, %eax
+	jle end_loop
+
+continou_read_loop:
+
+	### convert the buffer to upper case
+	pushl $BUFFER_DATA
+	pushl %eax
+	call to_upper
+	popl %eax			# get the size back
+	addl $4, %esp			# remove buffer pointer from stack
+
+	### write to output file
+	movl %eax, %edx			# size to edx
+	movl $SYS_WRITE, %eax
+	movl ST_FD_OUT(%ebp), %ebx
+	movl $BUFFER_DATA, %ecx
+	int $LINUX_SYSCALL
+
+	### continou the loop
+	jmp read_loop_begin
+
+end_loop:
+
+	### close files
+	movl $SYS_CLOSE, %eax
+	movl ST_FD_IN(%ebp), %ebx
+	int $LINUX_SYSCALL
+
+	movl $SYS_CLOSE, %eax
+	movl ST_FD_OUT(%ebp), %ebx
+	int $LINUX_SYSCALL
+
 exit_prog:
 	movl $1, %eax			# exit system call
+	movl $0, %ebx			# return value 0
 	int $0x80
 	
-	.type power, @function
-power:					# BEGIN FUNCTION
-	pushl %ebx			# 1. save old base pointer
+	#
+	# this function converts buffer to upper case asci
+	#
+	# variables:
+	#           %eax - beginning of buffer
+	#           %ebx - length of buffer
+	#           %edi - current offset in buffer
+	#           %cl  - current byte
+	#
+	.equ LOWERCASE_A, 'a'
+	.equ LOWERCASE_Z, 'z'
+	.equ UPPER_CONVERSION, 'A' - 'a'
+	.equ ST_BUFFER_LEN, 8
+	.equ ST_BUFFER, 12
+to_upper:				# BEGIN FUNCTION
+	pushl %ebp			# 1. save old base pointer
 	movl %esp, %ebp			# 2. make stack pointer the base pointer
-					
-	movl 8(%ebp), %ebx		# move 1. Parameter to %eax
-	movl 12(%ebp), %ecx		# move 2. Parameter to %ebx
-	pushl %ebx			# init local_var1 with base
-power_loop:
-	cmpl $1, %ecx			# while( power != 1)
-	je power_end
-	movl -4(%ebp), %eax		# local_var1 -> %eax
-	imull %ebx, %eax		# base * local_var1 
-	movl %eax, -4(%ebp)		# local_var1 := mul_result
-	decl %ecx			# power--
-	jmp power_loop
+
+	movl ST_BUFFER(%ebp), %eax
+	movl ST_BUFFER_LEN(%ebp), %ebx
+	movl $0, %edi			# i = 0
+
+	# if buffer empty, just leave
+	cmpl $0, %ebx
+	je to_upper_end
+
+loop:
+	movb (%eax,%edi,1), %cl		# %cl = BUFFER[i]
+
+	# unless char between 'a' and 'z' goto next byte
+	cmpb $LOWERCASE_A, %cl
+	jl next_byte
+	cmpb $LOWERCASE_Z, %cl
+	jg next_byte
+
+	# otherwise convert byte to uppercase
+	addb $UPPER_CONVERSION, %cl
+	# and store it back
+	movb %cl, (%eax,%edi,1)
+
+next_byte:
+	incl %edi			# i++
+	cmpl %edi, %ebx			# is end?
+	jne loop
 	
-power_end:	
+to_upper_end:
+					# no return value
 	movl %ebp, %esp			# restore stack pointer
 	popl %ebp			# restore base pointer
 	ret				# return control	
